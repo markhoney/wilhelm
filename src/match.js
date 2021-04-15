0b24ec04c47d7cc5daeec8229f73ddadc57a7bab
Mark Honeychurch <mark.honeychurch@telesmart.co.nz>
2021-04-15 17:11:17 +1200

    Changes

--- src/match.js
+++ src/match.js
@@ -1,3 +1,11 @@
+function similarity(one, two) {
+	let variance = 0;
+	for (const index of [1, 2, 3]) {
+		variance += Math.abs(one[index] - two[index]); // This might need weighting for each of the three values we're comparing
+	}
+	return variance;
+}
+
 function match(needle, haystack) {
 	for (const )
 }
