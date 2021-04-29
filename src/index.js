353769f0bbfe144d35c9e0ec03cfdfc14933c794
Mark Honeychurch <mark@honeychurch.org>
2021-04-29 22:36:11 +1200

    More clean up

---
+++ src/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+	config: require('./defaults'),
+	tests: require('./tests'),
+	load: require('./load'),
+	ft: require('./fft'),
+	points: require('./points'),
+	peaks: require('./peaks'),
+	print: require('./print'),
+	match: require('./match'),
+};
